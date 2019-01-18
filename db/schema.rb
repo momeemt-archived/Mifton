04b14fb15b1d807ca458cd2632f8a786a6150bc5
momeemt <me@momee.mt>
2019-01-18 15:16:03 +0900

    usersモデルとコントローラーの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_052245) do
+ActiveRecord::Schema.define(version: 2019_01_18_061355) do
 
   create_table "contests", force: :cascade do |t|
     t.text "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_01_18_052245) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
