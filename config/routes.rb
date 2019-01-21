53ad0b49b62cb32533b16e4234901cc17abc45b7
momeemt <me@momee.mt>
2019-01-21 21:01:25 +0900

    today commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/bector/new" => "bector#new"
   post "/bector/create" => "bector#create"
   get "/bector/:id" => "bector#show"
+  get "/users/index" => "users#index"
   get "/" => "home#index"
   get "/about" => "home#about"
   get "/privacypolicy" => "home#privacyPolicy"
