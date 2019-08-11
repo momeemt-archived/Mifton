a822d3cc0e333d88c76adf9fbe580b4345ca542d
momeemt <me@momee.mt>
2019-08-11 21:41:23 +0900

    🔨 fix syntax present > presence

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :title, present: true
-  validates :content, present: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
