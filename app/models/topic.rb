5a1155aff7ba77f0a5b8fe40788e6606e5123c47
momeemt <me@momee.mt>
2019-08-11 14:01:19 +0900

    ✨ add Topic Model トピックモデルを生成

---
+++ app/models/topic.rb
@@ -0,0 +1,6 @@
+class Topic < ApplicationRecord
+  belongs_to :user,
+  validates :user_id, presence: true
+  validates :title, present: true
+  validates :content, present: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_060818) do
+ActiveRecord::Schema.define(version: 2019_08_02_050526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,15 @@ ActiveRecord::Schema.define(version: 2019_07_30_060818) do
   create_table "tags", force: :cascade do |t|
     t.integer "micropost_id"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.string "image_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
