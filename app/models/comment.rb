322484d56663c5a31d2f2e864380412f0cd10d5d
momeemt <me@momee.mt>
2019-11-02 23:25:33 +0900

    ♻️ refactoring scss 永遠に終わらない scss コンポーネントわけ Bectorサイドバー

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, :foreign_key => "user_id"
+  #belongs_to :micropost, :foreign_key => "micropost_id"
   validates :user_id, presence: true
 end
